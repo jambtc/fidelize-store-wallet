57cdca823be0674c295991cf3f38ca3089f25f72
Sergio Casizzone <jambtc@gmail.com>
2021-03-01 12:44:29 +0100

    added new phpseclib

---
+++ protected/extensions/phpseclib/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c5ca4a3a47e66f6e985f3511b20d5c2e8992db1b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c5ca4a3a47e66f6e985f3511b20d5c2e8992db1b',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '2.0.30',
+      'version' => '2.0.30.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '136b9ca7eebef78be14abf90d65c5e57b6bc5d36',
+    ),
+  ),
+);
